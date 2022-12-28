6d37d5c9e827a1b6b6cb3c27f93c713cff7006dc
Nathan Woody <nathan@nathanwoody.com>
2022-12-28 05:37:08 +0000

    Commit to latest

--- src/frk_digitalout.py
+++ src/frk_digitalout.py
@@ -1,26 +1,19 @@
-from framework import Driver
-import digitalio
+from digitalio import DigitalInOut, DriveMode
 
-class DigitalOut(Driver):
-    _defaults = {'drive_mode': 'PUSH_PULL',
-                 'initial_value': False,
-                 'enable': True,
-                 'invert': False,
-                 'value': False}
-
-    _get_set_del = {'drive_mode': 'g',
-                    'initial_value': 'g',
-                    'enable': 'gs',
-                    'invert': 'gs',
-                    'value': 'gs'}
-
-    _drive_modes = {'NONE': None,
-                    'PUSH_PULL': digitalio.DriveMode.PUSH_PULL,
-                    'OPEN_DRAIN': digitalio.DriveMode.OPEN_DRAIN}
-
+class DigitalOut:
+    drive_mode = "PUSH_PULL"
+    initial_value = False
+    value = False
+    invert = False
+    enable = True
+    
+    _drive_modes = {"NONE": None,
+                    "PUSH_PULL": DriveMode.PUSH_PULL,
+                    "OPEN_DRAIN": DriveMode.OPEN_DRAIN}
+    
     def _init_device(self):
-        self._device = digitalio.DigitalInOut(self._pin)
+        self._device = DigitalInOut(self._pin)
         self._device.switch_to_output(value=self._initial_value, drive_mode=self._drive_modes[self._drive_mode])
-
+    
     def _set_value(self, v):
-        self._device.value = self._value = True if v else False
+        self._device.value = self._value = (bool(v) ^ self._invert) & self._enable
